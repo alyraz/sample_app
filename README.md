d10ee079d40860ccdf9f04c10b37a1c3f3116e07
Apprentice <apprentice@dbc09.local>
2013-03-12 14:30:29 -0700

    hugely improve the readme

---
+++ README.md
@@ -0,0 +1,4 @@
+--Start message. 
+Alyssa and Mary ROCK. 
+All you need to know. 
+--End message. 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'daa4eed03ab1e2c00a5e69103286ccd71e81e58748a1f8a6a4fd6b9b8a61f827953930e4ae34c98bb84337aa625c64e800f16ae477bfb9f264f3f4557acbb40e'
